2fce2986ec564c580b6926ca865a4128d506bb7d
Zeus Bernaga <contactomorehuman@gmail.com>
2025-03-18 16:57:12 -0600

    se trabajo las primeras constantes y funciones en JS referente a la aparicion y cerrar del popup

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,14 @@
 console.log("hola")
+
+const openButton = document.querySelector(".profile__edit");
+const closePopup = document.querySelector(".popup__close");
+
+openButton.addEventListener("click", function ( ) {
+  const popup = document.querySelector(".popup");
+popup.classList.add("popup__open")
+})
+
+closePopup.addEventListener("click", function (){
+  const popup = document.querySelector(".popup");
+popup.classList.remove("popup__open")
+})
