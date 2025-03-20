dcec3eec9560de50a5ffcec985524dd7415796ec
Zeus Bernaga <contactomorehuman@gmail.com>
2025-03-20 09:58:57 -0600

    se quitaron comentarios y reubicaron declaraciones en js para eficientar su lectura

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,30 @@
-console.log("hola")
+console.log("hola");
 
 const openButton = document.querySelector(".profile__edit");
 const closePopup = document.querySelector(".popup__close");
 
-const newNameInput = document.getElementById("name"); //<input/>
-const newJobInput = document.getElementById("info"); //<input/>
+const popup = document.querySelector(".popup");
+
+
+const newNameInput = document.querySelector("#name");
+const newJobInput = document.querySelector("#info");
 
 const profileName = document.querySelector(".profile__name");
 const profileDescription = document.querySelector(".profile__description");
 
-const formButton = document.querySelector(".popup__button"); //<button/>
+const formButton = document.querySelector(".popup__button");
 
+openButton.addEventListener("click", function () {
+  popup.classList.add("popup__open");
+});
 
-//este evento funcion activa el lapiz para traer el popup//
-openButton.addEventListener("click", function ( ) {
-  const popup = document.querySelector(".popup");
-popup.classList.add("popup__open")
-})
+closePopup.addEventListener("click", function () {
+  popup.classList.remove("popup__open");
+});
 
-//este evento funcion remueve el popup desde click en x //
-closePopup.addEventListener("click", function (){
-  const popup = document.querySelector(".popup");
-popup.classList.remove("popup__open")
-})
-
-//este evento debe realizar el cambio de la info en profile al rellenar yguardar el formulario//
-console.log(newJobInput)
+console.log(newJobInput);
 formButton.addEventListener("click", function (evt) {
-  evt.preventDefault(); //evita que la pagina se recargue al hacer submit
-  profileName.textContent = newNameInput.value
-  profileDescription.textContent = newJobInput.value
-
-})
-
-
-
-
+  evt.preventDefault(); 
+  profileName.textContent = newNameInput.value;
+  profileDescription.textContent = newJobInput.value;
+});
