980fb79ee1a155a2a18cfd3a197ecc5ca8098eb3
Zeus Bernaga <contactomorehuman@gmail.com>
2025-04-11 10:33:25 -0600

    funciones a boton de add, trash , like

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,7 @@
-console.log("hola");
-
 const openButton = document.querySelector(".profile__edit");
-const closePopup = document.querySelector(".popup__close");
+const closePopupPerfil = document.querySelector("#popup-close-perfil");
 
-const popup = document.querySelector(".popup");
+const popup = document.querySelector("#popup-perfil");
 
 const newNameInput = document.querySelector("#name");
 const newJobInput = document.querySelector("#info");
@@ -11,21 +9,111 @@ const newJobInput = document.querySelector("#info");
 const profileName = document.querySelector(".profile__name");
 const profileDescription = document.querySelector(".profile__description");
 
-const formButton = document.querySelector(".popup__button");
+const formButtonProfile = document.querySelector("#popupButtonProfile");
 
 openButton.addEventListener("click", function () {
   popup.classList.add("popup__open");
 });
 
-closePopup.addEventListener("click", function () {
+closePopupPerfil.addEventListener("click", function () {
   popup.classList.remove("popup__open");
 });
 
-formButton.addEventListener("click", function (evt) {
+formButtonProfile.addEventListener("click", function (evt) {
   evt.preventDefault();
   profileName.textContent = newNameInput.value;
   profileDescription.textContent = newJobInput.value;
   popup.classList.remove("popup__open");
+}); //pinta los nuevos valores mediante el textcontent y cierra el popup una vez sea rellenado el form//
+
+const openNuevoLugar = document.querySelector(".profile__add"); //llama desde el boton +//
+const nuevoTitulo = document.querySelector("#Titulo");
+const nuevoImagen = document.querySelector("#url");
+const popupAdd = document.querySelector("#popup-add"); //distingue por el add a su popup//
+const closePopupAdd = document.querySelector("#popup-close-add"); //llama la x para cerrar su popup//
+
+openNuevoLugar.addEventListener("click", function () {
+  popupAdd.classList.add("popup__open");
 });
 
+closePopupAdd.addEventListener("click", function () {
+  popupAdd.classList.remove("popup__open");
+}); //cierra desde x al popup//
 
+const initialCards = [
+  {
+    name: "Valle de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg",
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg",
+  },
+  {
+    name: "Montañas Calvas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg",
+  },
+  {
+    name: "Parque Nacional de la Vanoise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg",
+  },
+];
+const template = document.querySelector(".card");
+
+initialCards.forEach((itemCard) => {
+  const templateCard = document.querySelector(".template").content;
+  const card = templateCard.querySelector(".card").cloneNode(true);
+  const cardName = card.querySelector(".card__name");
+  cardName.textContent = itemCard.name;
+  const cardImage = card.querySelector(".card__image");
+  cardImage.src = itemCard.link;
+
+  const likeButton = card.querySelector("#like-button");
+  likeButton.addEventListener("click", function () {
+    if (likeButton.src == "http://127.0.0.1:5500/images/Vectorlike.png") {
+      likeButton.src = "./images/Vectorunlike.png";
+    } else {
+      likeButton.src = "./images/Vectorlike.png";
+    }
+  });
+  const container = document.querySelector(".elements"); //etiqueta padre//
+  container.append(card);
+});
+
+//tarjetas nuevas que salieron del popup//
+const formButtonAdd = document.querySelector("#popupButtonAdd");
+formButtonAdd.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  const templateCard = document.querySelector(".template").content;
+  const card = templateCard.querySelector(".card").cloneNode(true);
+  const cardName = card.querySelector(".card__name");
+  cardName.textContent = nuevoTitulo.value;
+  const cardImage = card.querySelector(".card__image");
+  cardImage.src = nuevoImagen.value;
+  const likeButton = card.querySelector("#like-button");
+  likeButton.addEventListener("click", function () {
+    likeButton.src = "./images/Vectorlike.png";
+  });
+  const likeButton = card.querySelector("#like-button");
+  likeButton.addEventListener("click", function () {
+    if (likeButton.src == "http://127.0.0.1:5500/images/Vectorlike.png") {
+      likeButton.src = "./images/Vectorunlike.png";
+    } else {
+      likeButton.src = "./images/Vectorlike.png";
+    }
+  });
+  const container = document.querySelector(".elements");
+  container.append(card);
+
+  popupAdd.classList.remove("popup__open");
+});
+
+const trashButton = card.querySelector("#trash-button");
